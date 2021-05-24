fc54570e9827b97dd915d5008ac2cc26cd80951c
mrmenndev <mrmenndev@protonmail.com>
2021-05-24 18:34:23 +0300

    version 1.3.0
    1.3.0 - 21.05.2021
    - Rewrite script
    - Remove ability to clear bloat-data

--- debloat.ps1
+++ debloat.ps1
@@ -8,92 +8,207 @@ $user_path = $env:USERPROFILE
 $apk_path = Join-Path $user_path "Downloads\*.apk"
 $adb_path = Join-Path $user_path "Documents\platform-tools"
 
-#==========functions==========
-$version = "1.2.1"
 $name = $MyInvocation.MyCommand.Name
-$running = $true
-$bloat_list = @()
+$version = "1.3.0"
 
-Function Packages {
+#==========functions==========
+
+function parseFile {
+    $bloat_list = @()
+    $raw = Get-Content $bloat_file
+
+    $raw.ForEach({
+        if ($_.startswith("#")){
+        }
+        elseif ($_.Length -lt 3){
+        }
+        else{
+            $bloat_list += $_
+        }
+    })
+
+    return $bloat_list
+}
+
+function listPackages {
+    Write-Host "read ADB packages"
+
     $packages = @()
-    Write-Host "read adb packages"
-    $adb_out_packages = & $adb shell "pm list packages"
+    $packages_raw = (& $adb shell "pm list packages")
 
-    if ($adb_out_packages -eq $null){
-        Write-Host $adb_out_packages
-        Kill_ADB
+    if ($packages_raw -eq $null){
+        Write-Host $packages_raw
+        killADB
     }
 
     # cleanup package result
-    $adb_out_packages.ForEach({
+    $packages_raw.ForEach({
         $packages += $_.Replace("package:","")
     })
 
     return $packages
 }
 
-Function Kill_ADB {
+function connectRemote {
+    Write-Host "connect to $remote_ip"
+
+    $adb_message = [string] (& $adb connect $remote_ip ) 2>$null
+
+    if($adb_message.startswith("connected to")){
+        Write-Host "connected to $remote_ip"
+    }
+    else{
+        Write-Host $adb_message
+        killADB
+    }
+}
+
+function killADB {
     Write-Host "Kill Adb-Server"
+
     & $adb kill-server
     Exit
 }
 
-Function Promt_Back {
-    Write-Host "========================================"
-    $promt_inner = Read-Host 'Press enter to go back or press 0 to exit'
-    if ($promt_inner -eq 0) {
-        Kill_ADB
+function promtBack {
+    $promt = Read-Host 'Press enter to go back or press 0 to exit'
+
+    if ($promt -eq 0) {
+        killADB
     }
     else {
         Clear-Host
     }
 }
 
-Function Bloat_Installed {
-    $packages = Packages
-    $bloat_installed = $bloat_list | Where {$packages -Contains $_}
+#==========init==========
+
+Write-Host "read $bloat_file"
+
+# check if bloat-file is there
+if (-not ($bloat_file | Test-Path)){
+    Write-Host "$bloat_file can't be found"
+    Exit
+}
+
+# read bloat-file
+$bloat_list = parseFile
+
+#----------
+# check if ADB path is correct
+Write-Host "check if ADB path is correct"
+
+$adb = Join-Path $adb_path adb.exe
+if (-not ($adb | Test-Path)){
+    Write-Host "ADB can't be found"
+    Exit
+}
+
+$adb_info = (& $adb version)
+
+# connect to remote if remote_ip is available
+if (-not ($remote_ip -eq $Null)){
+    connectRemote
+}
+
+#----------
+# check if ADB can list packages
+$test_packages = listPackages
+
+Write-Host "Initial setup complete"
+
+#==========menu==========
+
+function showInfo {
+    Write-Host $name
+    Write-Host "version: $version"
+    Write-Host "bloat-file: $bloat_file"
+    Write-Host "--------------------------"
+    Write-Host "ADB:"
+    $adb_info.Foreach({
+        Write-Host $_
+    })
     Write-Host "========================================"
-    Write-Host "Following bloatware are installed: "
+}
+
+function installAPKs {
+    # list apks
+    $apk_files = Get-ChildItem -Path $apk_path
+    $apk_path_raw = Split-Path -Path $apk_path
+
+    Write-Host "Following apks has been found in:"
+    Write-Host $apk_path_raw
+    Write-Host "========================================"
+    $apk_files.ForEach({
+        Write-Host $_.Basename
+    })
+    Write-Host "========================================"
+    $promt = Read-Host 'Press 1 to install or press enter to go back'
+    
+    # install apks
+    if (($promt -eq 1) -and ($apk_files.Count -gt 0)) {
+        $apk_files.Foreach({
+            Write-Host "Install $($_.Basename)"
+            & $adb install $_
+        })
+        promtBack
+    }
+    else{
+        Clear-Host
+    }
+}
+
+function showBloatInstalled {
+    $packages = listPackages
+    $bloat_installed = $bloat_list | Where {$packages -Contains $_}
+
+    Write-Host "========================================"
+    Write-Host "Following bloatware are installed:"
     Write-Host "========================================"
     $bloat_installed.Foreach({
         Write-Host $_
     })
     Write-Host "========================================"
     Write-Host "Count: $($bloat_installed.Count)"
-    
+    Write-Host "========================================"
 }
 
-Function Bloat_Not_Installed {
-    $packages = Packages
+function showBloatNotInstalled {
+    $packages = listPackages
     $bloat_not_installed = $bloat_list | Where {$packages -NotContains $_}
+
     Write-Host "========================================"
-    Write-Host "Following bloatware found in '$bloat_file'" 
-    Write-Host "but are not installed: "
+    Write-Host "Following bloatware found in $bloat_file" 
+    Write-Host "but are not installed:"
     Write-Host "========================================"
     $bloat_not_installed.Foreach({
         Write-Host $_
     })
     Write-Host "========================================"
     Write-Host "Count: $($bloat_not_installed.Count)"
+    Write-Host "========================================"
 }
 
-Function List_Installed_Packages {
-    $packages = Packages
+function showInstalledPackages {
+    $packages = listPackages
+
     Write-Host "========================================"
-    Write-Host "Following packages are installed: "
+    Write-Host "Following packages are installed:"
     Write-Host "========================================"
     $packages.Foreach({
         Write-Host $_
     })
     Write-Host "========================================"
     Write-Host "Count: $($packages.Count)"
+    Write-Host "========================================"
 }
 
-Function Uninstall_Bloat {
-    $packages = Packages
+function uninstallBloat {
+    $packages = listPackages
     $bloat_installed = $bloat_list | Where {$packages -Contains $_}
+    
     Write-Host "========================================"
-    Write-Host "Uninstall bloatware"
+    Write-Host "Uninstall bloatware..."
     Write-Host "========================================"
     $success = 0
     $count = $bloat_installed.Count
@@ -109,135 +224,18 @@ Function Uninstall_Bloat {
     })
     Write-Host "========================================"
     Write-Host "Bloatware uninstalled: $success"
-}
-
-Function Clear_Bloat_Data{
     Write-Host "========================================"
-    Write-Host "Clear data from bloatware"
-    Write-Host "========================================"
-    $success = 0
-    $count = $bloat_list.Count
-    $bloat_list.Foreach({
-        $adb_message = [string] (& $adb shell "pm clear" $_ ) 2>$null
-        if ($adb_message -eq "Success"){
-            $success += 1
-            Write-Host "($success/$count) Cleared: $_ "
-        }
-        else {
-            Write-Host "Failed to clear: $_"
-        }
-    })
-    Write-Host "========================================"
-    Write-Host "Bloatware cleared: $success"
 }
 
-Function Install_APKS(){
-    # List apks
-
-    $apk_list = Get-ChildItem -Path $apk_path
-    $apk_path_raw = Split-Path -Path $apk_path
-    
-    Write-Host "Following apks are found in"
-    Write-Host $apk_path_raw
-    Write-Host "========================================"
-    $apk_list.ForEach({
-        Write-Host $_.Basename
-    })
-    # Propt to install apk
-    Write-Host "========================================"
-    $promt_inner = Read-Host 'Press 1 to install or press enter to go back'
-    if ($promt_inner -eq 1) {
-        $apk_list.Foreach({
-            # install apks
-            Write-Host "Install $($_.Basename)"
-            & $adb install $_
-        })
-    }
-    else{
-        Clear-Host
-    }
-}
-
-Function Show_Info {
-    Write-Host "========================================"
-    Write-Host $name
-    Write-Host "version: $version"
-    Write-Host "bloat-file: $bloat_file"
-    Write-Host "--------------------------"
-    Write-Host "adb:"
-    $adb_info.Foreach({
-        Write-Host $_
-    }) 
-}
-
-#==========initial setup==========
-
-# check if adb path is correct
-Write-Host "check if adb path is correct"
-
-$adb = Join-Path $adb_path adb.exe
-if (-not ($adb | Test-Path)){
-    Write-Host "adb can't be found"
-    Exit
-}
-$adb_info =  & $adb version
-
-# connect to remote ip if available
-if (-not ($remote_ip -eq $Null)){
-    Write-Host "connect to $remote_ip"
-    $adb_message = [string] (& $adb connect $remote_ip ) 2>$null
-    if($adb_message.startswith("connected to")){
-        Write-Host "connected to $remote_ip"
-    }
-    else{
-        Write-Host $adb_message
-        Kill_ADB
-    }
-}
-
-#----------
-# check if debloat_list.txt is there
-Write-Host "check if the bloat list called $bloat_file is there"
-
-if (-not ($bloat_file | Test-Path)){
-    Write-Host "$bloat_file can't be found"
-    Exit
-}
-
-#----------
-# read bloat file
-Write-Host "read $bloat_file"
-
-$bloat_list_raw = Get-Content $bloat_file
-
-$bloat_list_raw.ForEach({
-    if ($_.startswith("#")){
-    }
-    elseif ($_.Length -lt 3){
-    }
-    else{
-        $bloat_list += $_
-    }
-})
-
-#----------
-# check if adb can list packages
-$test_packages = Packages
-
-Write-Host "Initial setup complete"
-
-#==========menu==========
-
-while ($running -eq $true){
+while ($true){
     Write-Host "========================================"
     Write-Host "Menu:"
     Write-Host "1.) bloatware - installed"
     Write-Host "2.) bloatware - not installed"
     Write-Host "3.) bloatware - uninstall all"
-    Write-Host "4.) bloatware - clear data"
-    Write-Host "5.) list installed packages"
-    Write-Host "6.) install apks"
-    Write-Host "7.) info"
+    Write-Host "4.) list installed packages"
+    Write-Host "5.) install apks"
+    Write-Host "6.) info"
     Write-Host "0.) exit"
     Write-Host "========================================"
 
@@ -246,34 +244,30 @@ while ($running -eq $true){
 
     switch ($promt){
         1{
-            Bloat_Installed
-            Promt_Back
+            showBloatInstalled
+            promtBack
         }
         2{
-            Bloat_Not_Installed
-            Promt_Back
+            showBloatNotInstalled
+            promtBack
         }
         3{
-            Uninstall_Bloat
-            Promt_Back
+            uninstallBloat
+            promtBack
         }
         4{
-            Clear_Bloat_Data
-            Promt_Back
+            showInstalledPackages
+            promtBack
         }
         5{
-            List_Installed_Packages
-            Promt_Back
+            installAPKs
         }
         6{
-            Install_APKS
-        }
-        7{
-            Show_Info
-            Promt_Back
+            showInfo
+            promtBack
         }
         0{
-            Kill_ADB
+            killADB
         }
     }
 }
