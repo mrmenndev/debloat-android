449004331874387fe9645cb02b97e684c5f2ff8b
mrmenndev <mrmenndev@protonmail.com>
2021-05-31 15:25:19 +0300

    Add [release] script

---
+++ release.ps1
@@ -0,0 +1,13 @@
+$version="1.5.1"
+
+$current = Get-Location
+$root = $current #(Split-Path $current)
+
+$files = @(
+        (Join-Path $root "config"),
+        (Join-Path $root "debloat.ps1")
+        (Join-Path $root "download_adb.ps1"))
+
+$output = "debloat_android_v$version.zip"
+
+Compress-Archive -Force -Path $files -DestinationPath $output
